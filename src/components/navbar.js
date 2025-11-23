bc24071d21bcbe77d12f16db442b822fe83261a1
EyadManaa <ayad2552005@gmail.com>
2025-11-23 10:50:37 +0200

    this is the second change

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/navbar.css'; // import the CSS
+import '../styles/navbar.css'; 
 import logo from '../assets/logo.png';
 
 const Navbar = () => {
